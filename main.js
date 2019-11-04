f6e1e6a15c8734367f9e931f381787f304a5b15c
daniel.falklimberg <Daniel.falklimberg@gmail.com>
2019-11-04 09:31:29 +0100

    v0.013

--- main.js
+++
@@ -1,9 +0,0 @@
-$("#gallery-container>.row>section").hover(function () {
-    $(this).children(".description").css({
-        "bottom": "0"
-    })
-}, function () {
-    $(this).children(".description").css({
-        "bottom": "-30%"
-    })
-})
